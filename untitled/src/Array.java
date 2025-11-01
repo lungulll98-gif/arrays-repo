2102546dbaf1f23ef8dbf577334f313dbeb99a11
Lungu Victor <lungulll98@gmail.com>
2025-11-01 11:44:13 +0200

    Add new exemplu

---
+++ untitled/src/Array.java
@@ -0,0 +1,21 @@
+public class Array {
+    public static void main(String[] args) {
+
+        int zile_pe_luna[];
+        zile_pe_luna = new int[12];
+        zile_pe_luna[0] = 31;
+        zile_pe_luna[1] = 28;
+        zile_pe_luna[2] = 31;
+        zile_pe_luna[3] = 30;
+        zile_pe_luna[4] = 31;
+        zile_pe_luna[5] = 308;
+        zile_pe_luna[6] = 31;
+        zile_pe_luna[7] = 31;
+        zile_pe_luna[8] = 30;
+        zile_pe_luna[9] = 31;
+        zile_pe_luna[10] = 30;
+        zile_pe_luna[11] = 31;
+        System.out.println("In noiembrie sunt " + zile_pe_luna[10] + " zile.");
+
+    }
+}
